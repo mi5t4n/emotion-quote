d4ac9a01426e288bc8c292147af5267f69b22dda
Sagar Tamang <mi5t4n@gmail.com>
2019-01-06 20:34:27 +0545

    implemented the canvas and the fetched the face data form Microsoft API.

--- app.js
+++ app.js
@@ -3,32 +3,31 @@ var interval;
 
 jQuery(document).ready(function(e){
 
+  Webcam.set({
+    image_format: 'jpeg',
+    jpeg_quality: 100,
+    force_flash: false
+});
+
   Webcam.attach('#camera_video');
 
 });
 
 jQuery("#btn_snap").click(function(e){
-  interval = setInterval(take_snapshot, 1000);
+  take_snapshot();
 });
 
 function take_snapshot(){
-  var max_time = jQuery("#selfie_interval option:selected").val();
-  ++index;
-  if (index > max_time) {
-    closeInterval(interval);
 
-    Webcam.snap(function(data_uri){
-      jQuery("#camera_picture").html( '<img src="' + data_uri + '"/>' );
-      processImage(data_uri);
-    });
-  }
+  Webcam.snap(function(data_uri){
+    jQuery("#camera_picture").html( '<img src="' + data_uri + '"/>' );
+    loadCanvasWithDataURI(data_uri);
+    sendImage();
+  });
 
-  jQuery("#interval_show").html(index);
-
-  
 }
 
-function processImage(data_uri) {
+function sendImage() {
   var subscriptionKey = "f343fcd01e184711a7fa7b188ed5759e";
 
   var uriBase = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect";
@@ -42,23 +41,76 @@ function processImage(data_uri) {
     "hair,makeup,occlusion,accessories,blur,exposure,noise"
   };
 
-  // Perform the REST API call.
-  jQuery.ajax({
-    url: uriBase + "?" + jQuery.param(params),
+  var canvas = document.getElementById('camera_canvas');
+  canvas.toBlob(function(blob){
 
-    // Request headers.
-    beforeSend: function(xhrObj){
-      xhrObj.setRequestHeader("Content-Type","application/json");
-      xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
-    },
+    // Perform the REST API call.
+    jQuery.ajax({
+      url: uriBase + "?" + jQuery.param(params),
+      type: "POST",
+      cache: false,
+      processData: false,
+      contentType: 'application/octet-stream',
 
-    type: "POST",
+      // Request headers.
+      beforeSend: function(xhrObj){      
+        xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+      },
 
-    // Request body.
-    data: '{"url": ' + '"' + data_uri + '"}',
-  }).done(function(data){
-    console.log(data);
-  }).fail(function(jqXHR, textStatus, errorThrown){
+      // Request body.
+      data: blob
 
-  });
+    }).done(function(data){
+      console.log(data);
+      if (data.length == 0) {
+        jQuery('#emotion-modal').modal('toggle');
+      } else {
+        x = data[0].faceRectangle.left;
+        y = data[0].faceRectangle.top;
+        width = data[0].faceRectangle.width;
+        height = data[0].faceRectangle.height;
+
+        var canvas = document.getElementById('camera_canvas');
+        var context = canvas.getContext('2d');
+        context.rect(x, y, width, height);
+        context.lineWidth = "4";
+        context.strokeStyle = "green";
+        context.stroke();
+      }
+    }).fail(function(jqXHR, textStatus, errorThrown){
+      //
+    });
+  }, 'image/jpeg', 1);
+  
 }
+
+// Load canvas with data URI
+function loadCanvasWithDataURI(dataURI) {
+
+  var canvas = document.getElementById('camera_canvas');
+  var context = canvas.getContext('2d');
+
+  // load image from data URI
+  var imageObj = new Image();
+  imageObj.onload = function() {
+    context.drawImage(this, 0, 0);
+  };
+
+  imageObj.src = dataURI;
+
+}
+
+// Convert data URI to Blob
+dataURItoBlob = function(dataURI) {
+  var array, binary, i;
+  binary = atob(dataURI.split(",")[1]);
+  array = [];
+  i = 0;
+  while (i < binary.length) {
+    array.push(binary.charCodeAt(i));
+    i++;
+  }
+  return new Blob([new Uint8Array(array)], {
+    type: "image/jpeg"
+  });
+};
